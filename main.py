9045f7643e3cf94a67ef9af1b1cb320f1eaa4fb3
Barsik-ai5 <egorcukanov400@gmail.com>
2026-09-05 17:14:43 +0300

    main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ SOURCES = ["@vrv_radar", "@radar_ru_belgorod", "@locatorru"]
 
 genai.configure(api_key=GEMINI_API_KEY)
 # Используем flash для молниеносной скорости реакции
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 app = Client("radar_bot", session_string=SESSION_STRING, api_id=API_ID, api_hash=API_HASH)
 
